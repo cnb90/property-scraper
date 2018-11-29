e7d91dd16999a053aa7a33596f92fb8969ffc28b
cnb90 <cnickbock@gmail.com>
2018-11-28 20:51:21 -0500

    Improving the readability of the code

--- PropertyScraper.py
+++ PropertyScraper.py
@@ -9,35 +9,46 @@ def main():
     final_list = []
     lines = []
 
+    # Create session to get all properties
     s = requests.Session()
     r = s.get(all_properties_url)
     soup = BeautifulSoup(r.text, "html.parser")
+
+    # Get all properties
     property_ids = soup.find_all("td", attrs={"class": "hidden-print"})
+
     for property_id in property_ids:
-        print("Property Found"),
+        # Create the property url
         property_id = str(property_id).split("PropertyId=", 1)[1].split('\"')[0]
         property_url = single_property_url + property_id
+        print("Property Found"),
+
+        # Get the page data of the property
         property_page = s.get(property_url)
         property_page = BeautifulSoup(property_page.text, "html.parser")
-        print(">"),
-
         t_info = property_page.find_all("table")[0]
         t_status = property_page.find_all("table")[1]
+        print(">"),
 
+        # Filter out the unneeded and keep the good data
         for line in t_info:
             info = str(line).strip('<tr>').strip('</tr>').replace('<td>','').replace('</td>','').replace('\n','')
             if info != '':
                 lines.append(info)
             print(">"),
 
+        # Organize property info into a dictionary
         d = OrderedDict()
+
+        # Get property status and last status date
         for status in t_status:
             status = str(status).strip('<tr>').strip('</tr>').replace('\n', '').replace('</td><td>', ' : ').rstrip()
             if status != '' and status.startswith("<td>"):
-                d['Latest Status'] = status.split(" : ")[0].strip("<td>")
+                d['Latest Status'] = status.split(" : ")[0].strip("<td>") + "d"
                 d['Latest Status Date'] = status.split(" : ")[1].strip("</td>")
                 break
 
+        # Get sales date, sheriff #, address, and debt amount of property
         for line in lines:
             if "Sales Date :" in line:
                 d['Sales Date'] = line.split(":")[1]
@@ -48,14 +59,26 @@ def main():
             elif "Debt Amount :" in line:
                 d['Debt Amount'] = line.split(":")[1]
 
-        print(">")
+        # Get property url
         d['Link'] = property_url
+
+        # Copy the contents of the property info dictionary into a list
         final_list.append(d.copy())
         lines = []
         d.clear()
+        print(">")
 
+    # create csv with all properties
     with open('test.csv', 'wb') as csv_file:
-        fieldnames = ['Sheriff #', 'Sales Date', 'Latest Status', 'Latest Status Date', 'Address', 'Debt Amount', 'Link']
+        fieldnames = [
+            'Sheriff #',
+            'Sales Date',
+            'Latest Status',
+            'Latest Status Date',
+            'Address',
+            'Debt Amount',
+            'Link'
+        ]
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(final_list)
